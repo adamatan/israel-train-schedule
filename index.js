b93f5f84e835c52f3e1508476e2893703e46631e
Adam Matan <adam@matan.name>
2017-12-12 09:47:56 +0200

    Executable script

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+#!/usr/bin/env node
+
 const puppeteer = require('puppeteer');
+const util = require('util');
 
 const date = new Date();
 const dateStr = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
+const originStation = 4600;
+const destinationStation = 5900;
 
-const URL = `https://www.rail.co.il/en/pages/trainsearchres.aspx?FSID=5900&TSID=4600&Date=${dateStr}&Hour=0000&IOT=true&IBA=false`;
+const URL = `https://www.rail.co.il/en/pages/trainsearchres.aspx?FSID=${originStation}&TSID=${destinationStation}&Date=${dateStr}&Hour=0000&IOT=true&IBA=false`;
 const regex = /departure\stime\s+([\d:]+)[\s\S]*arrival\stime\s+([\d:]+)[\s\S]*Platform\s+(\S+)[\s\S]*Duration\s([\d:]+)/m;
 
 function parseLine(line) {
@@ -14,21 +19,24 @@ function parseLine(line) {
   return {};
 }
 
-
+function formatLine(line) {
+  return util.format('%s-%s (%s)', line.departure, line.arrival, line.duration);
+}
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(URL);
-  await page.screenshot({ path: 'screenshot.png' });
   const lines = await page.evaluate(() => {
     const anchors = Array.from(document.querySelectorAll('.trainBasicInfo'));
     return anchors.map(anchor => anchor.textContent);
   });
 
-  let i = 0;
-  for (i = 0; i < lines.length; i++) {
-    console.log(parseLine(lines[i]));
+  const departures = Array.from(new Set(lines.map(line => formatLine(parseLine(line)))));
+  departures.sort();
+  let i;
+  for (i = 0; i < departures.length; i += 1) {
+    console.log(departures[i]);
   }
   await browser.close();
 })();
